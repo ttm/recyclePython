fe28d067d70c930ad982fe5e39dd617f4430e080
Renato Fabbri <renato.fabbri@gmail.com>
2015-02-24 16:38:38 -0300

    terminado tutorial de python 3, caindo para outras documentacoes

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ há quase 10 anos, e desde então não fiz uma leitura cuidadosa do material
 básico, nem me atualizei para além dos casos triviais.
 
 ## Python 3
-A pasta py3/ possui anotações de uma leitura de todo tutorial oficial da linguagem (3.4.3rc1) para atualizar do Python 3.
+A pasta py3/ possui anotações de uma leitura de todo tutorial oficial da linguagem (3.4.3rc1), e documentações adicionais sobre pacotes, para atualizar do Python 3.
 
 ## IPython
 A pasta ipy/ possui anotações de uma leitura do material básico sobre o ipython.
@@ -16,4 +16,4 @@ a pasta spy/ possui anotações de uma visita aos recursos do Scipy.
 
 ## Anotações sobre novos usos
 
-- Criação de módulos caprichados e bem documentados para as pesquisas, de forma que o código seja melhor reutilizável por mim e comunidade. Cuidados especiais: docstrings, erros corretos, anotações de uso, incertezas e ToDo. O detalhamento deve ser acompanhado da criação de uma documentação de forma automatizada, e.g. com o uso do Doxygen.
+- Criação de módulos caprichados e bem documentados para as pesquisas, de forma que o código seja melhor reutilizável por mim e comunidade. Cuidados especiais: docstrings, lidas acertadas com erros, anotações de uso, incertezas, testes e ToDo. O detalhamento deve ser acompanhado da criação de uma documentação de forma automatizada, e.g. com o uso do Doxygen. Também da disponibilização para o Python Package Index. Desenvolver projeto no development mode, e pyvenv.
--- py3/newThings.py
+++ py3/newThings.py
@@ -135,3 +135,178 @@ dir() # exibe todos os nomes do namespace, para alem do %whos
 # dentro do pacote, o import usa caminhos absolutos import xxx.yyy.zzz
 # caminhos relativos também funcionam from ..yyy import zzz,
 # mas dão problema em algumas situações
+
+# formatação com {}:
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+for name, phone in table.items():
+    print('{0:10} ==> {1:10d}'.format(name, phone))
+
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 8637678}
+print('Jack: {0[Jack]:d}; Sjoerd: {0[Sjoerd]:d}; '
+      'Dcab: {0[Dcab]:d}'.format(table))
+
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 8637678}
+print('Jack: {Jack:d}; Sjoerd: {Sjoerd:d}; '
+      'Dcab: {Dcab:d}'.format(**table))
+
+# vars() retorna um dicionário das variáves definidas até o momento
+
+# para formatação com o %, ñ é mais necessário a tupla
+import math
+print('The value of PI is approximately %5.3f.' % math.pi)
+print('The value of PI is approximately %5.3f.' % (math.pi,))
+
+with open('workfile.txt', 'r') as f:
+    read_data = f.read()
+print(f.closed, read_data)
+
+# JSON
+import json
+x=[1,"simple","list"]
+
+with open('jsonfile.txt', 'w') as f:
+    json.dump(x,f)
+with open('jsonfile.txt', 'r') as f:
+    x2=json.load(f)
+print(x,x2,json.dumps(x))
+
+try:
+    asoidj
+except:
+    print("info sobre o que tah rolando")
+    #raise
+    print("error is:", sys.exc_info())
+
+try:
+    a=1
+except:
+    print("error is:", sys.exc_info())
+else:
+    print("nada errado no try!")
+
+try:
+   raise Exception('spam', 'eggs')
+except Exception as inst:
+   print(type(inst))    # the exception instance
+   print(inst.args)     # arguments stored in .args
+   print(inst)          # __str__ allows args to be printed directly,
+                        # but may be overridden in exception subclasses
+   x, y = inst.args     # unpack args
+   print('x =', x)
+   print('y =', y)
+
+# In real world applications, the finally clause is useful for releasing 
+# external resources (such as files or network connections),
+# regardless of whether the use of the resource was successful.
+try:
+    raise NameError('HiThere')
+except NameError:
+    print('An exception flew by!')
+    #raise
+    print("error is:", sys.exc_info())
+finally:
+    print("alway execute his clause")
+
+import __main__
+dir(__main__) # libera uma relação de objetos definidos na sessão
+
+def scope_test():
+    def do_local():
+        spam = "local spam"
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+    def do_global():
+        global spam
+        spam = "global spam"
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
+
+class MyClass:
+    def myFunction(self):
+        pass
+mc=MyClass()
+print(mc.myFunction,type(mc.myFunction),MyClass.myFunction, type(MyClass.myFunction))
+
+class Dog:
+
+    tricks = []             # mistaken use of a class variable
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.add_trick('roll over')
+e.add_trick('play dead')
+print(d.tricks)
+
+# a iteração sobre o dicionário entrega as chaves:
+for i in {"ak": 4, 5:2}:
+    print(i)
+
+class Reverse:
+    """Iterator for looping over a sequence backwards."""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev = Reverse('spam')
+iter(rev)
+for char in rev:
+    print(char)
+
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
+
+for char in reverse('golf'):
+    print(char)
+
+import shutil, glob
+import getopt, argparse
+import math, random
+import timeit, profile, pstats
+print("timeit x=10",timeit.Timer("x=10").timeit(),"timeit x=10",timeit.Timer("x=10").timeit(),"timeit x=1000",timeit.Timer("x=1000").timeit(),"\n---> timeit x=10",timeit.Timer("x=10").repeat(),)
+import doctest, unittest
+import reprlib, pprint, textwrap
+
+# The principal challenge of multi-threaded applications is coordinating threads that share data or other resources. To that end, the threading module provides a number of synchronization primitives including locks, events, condition variables, and semaphores.
+import threading, queue
+
+# para fazer o logfile certinho, não ficar printando tudo no meio do escrito para manter 
+# registro do que acontece
+import logging
+
+import weakref, gc
+
+from array import array
+a = array('H', [4000, 10, 700, 22222])
+
+import bisect
+import decimal
+
+#what if you can’t install packages into the global site-packages directory? For instance, on a shared host.
+# In all these cases, virtual environments can help you. pyvenv
+
+# https://github.com/pypa/sampleproject/blob/master/setup.py
+# http://docutils.sourceforge.net/docutils/statemachine.py
+# https://packaging.python.org/en/latest/distributing.html#creating-your-own-project
